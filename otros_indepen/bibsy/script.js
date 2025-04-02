9668b4f4c5662089181319d9da62686b204ffa9c
sponky <inge.infoclass23@gmail.com>
2025-04-02 04:48:26 -0300

    modificacio

--- otros_indepen/bibsy/script.js
+++ otros_indepen/bibsy/script.js
@@ -9,3 +9,14 @@ control.addEventListener('input', (event) => {
   }
   imagen.style.transform = `translate(-50%, -${translateValue}%)`; // Solo mover en el eje Y
 });
+
+// Bloquear desplazamiento horizontal en móviles
+document.addEventListener('touchmove', function(event) {
+  if (event.scale !== 1) {
+    event.preventDefault(); // Evita el zoom con dos dedos
+  }
+}, { passive: false });
+
+window.addEventListener('scroll', function() {
+  window.scrollTo(0, window.scrollY); // Bloquea el desplazamiento horizontal
+});

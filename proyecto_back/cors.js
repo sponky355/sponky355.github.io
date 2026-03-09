207819db44c3fafc76753912b0a16bc191d61d03
sponky <inge.infoclass23@gmail.com>
2026-03-08 22:17:14 -0300

    Add files via upload

---
+++ proyecto_back/cors.js
@@ -0,0 +1,8 @@
+[
+  {
+    "origin": ["https://sponky355.github.io"],
+    "method": ["GET", "POST", "PUT", "DELETE"],
+    "maxAgeSeconds": 3600,
+    "responseHeader": ["Content-Type", "Authorization"]
+  }
+]
